ee0427dd4c87bcbd64e725caa2d13b1b420d5383
Driztaker <prom6381@gmail.com>
2026-06-22 15:41:02 +0100

    12

--- enemies/enemy_spawner.gd
+++ enemies/enemy_spawner.gd
@@ -1,7 +1,6 @@
 extends Node3D
 
 var enemy_scene = preload("res://enemies/enemy.tscn")
-
 var timer = 0.0
 
 func _process(delta):
@@ -11,7 +10,5 @@ func _process(delta):
 		timer = 0.0
 
 		var enemy = enemy_scene.instantiate()
-
-		enemy.position = position
-
 		get_parent().add_child(enemy)
+		enemy.position = position
