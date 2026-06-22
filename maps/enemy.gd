0d8b78db2b010601c041c980a477966079facbf1
Driztaker <prom6381@gmail.com>
2026-06-22 14:57:58 +0100

    12

--- maps/enemy.gd
+++
@@ -1,9 +0,0 @@
-extends Node3D
-
-@onready var goal = get_node("../Goal")
-
-var speed = 2.0
-
-func _process(delta):
-	var direction = (goal.position - position).normalized()
-	position += direction * speed * delta
