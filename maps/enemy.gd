e6e722172c14c75001492bd48757d4c219026b16
Driztaker <prom6381@gmail.com>
2026-06-22 14:32:53 +0100

    Enemy movement

--- maps/enemy.gd
+++ maps/enemy.gd
@@ -1,4 +1,9 @@
 extends Node3D
 
+@onready var goal = get_node("../Goal")
+
+var speed = 2.0
+
 func _process(delta):
-	position.x += 2 * delta
+	var direction = (goal.position - position).normalized()
+	position += direction * speed * delta
